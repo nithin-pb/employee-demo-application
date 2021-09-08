167b91c80c9a5fc2965fff52d2ed37b652fc6128
Nithin PB <nithinpb180@gmail.com>
2021-09-08 09:35:23 +0530

    Heroku changes

--- src/service/index.js
+++ src/service/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:8000'
+const BASE_URL = 'https://secret-cove-15132.herokuapp.com/'
 export default {
     employee() {
         return {
